7bd36077b79395a85f8eb2599f232ae649dbdc49
Glenn De Cooman <glenn.decooman@hotmail.be>
2021-05-24 17:09:12 +0200

    Add project

---
+++ src/main/java/be/glenndecooman/villagergroupdiscount/VillagerGroupDiscount.java
@@ -0,0 +1,15 @@
+package be.glenndecooman.villagergroupdiscount;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VillagerGroupDiscount extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
